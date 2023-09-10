e069ddda6ee9eca67171b92c590bcdc00ed83855
Matt-Fr <matthieu_franck@orange.fr>
2023-09-10 17:43:25 +0200

    added external component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.experiments = { topLevelAwait: true };
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "main-app",
+        remotes: {
+          remote_component: `remotecomponent@http://localhost:3001/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/primaryEntry.js",
+        exposes: {},
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
