769daca4d15435943978164b5f1044d3973664ca
Matt-Fr <matthieu_franck@orange.fr>
2023-09-13 10:18:02 +0200

    add a navbar icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,33 @@
-import React from "react";
-import { Typography, AppBar, Toolbar } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Image from "next/image";
+import HomeIcon from "@mui/icons-material/Home"; //
+import Link from "next/link";
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <AppBar position="relative">
+    <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" color="inherit" noWrap>
-          Album layout
+        <Link href="/">
+          <IconButton
+            sx={{
+              color: "white",
+            }}
+          >
+            <HomeIcon />
+          </IconButton>
+        </Link>
+
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Ecommerce Project
         </Typography>
+        <IconButton color="inherit" aria-label="cart">
+          <ShoppingCartIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
-};
-
-export default Navbar;
+}
